5422d4d9043eac45a3fa5fd290ea056334653510
sa <alvarengasergio@hotmail.co.uk>
2021-10-01 17:01:37 +0200

    Removed comment

--- models/emergency.js
+++ models/emergency.js
@@ -36,30 +36,5 @@ const emergencySchema = new Schema({
     },
 })
 
-/* 
-    FOR ADDING IN A VOLUNTEER TO THE EMERGENCY 
-
-    const makeEmergency = async()=>{
-        const em = new Emergency({
-            description: 'Bla bla bla',
-            city: 'Bla',
-            street: 'Bla Bla Bla',
-        })
-
-        //IMPORTANT LINE
-        em.volunteer.push({
-            firstName: 'Raul',
-            lastName: 'Börger',
-            phone: 12345678
-        })
-        const savedEmergency = em.save();
-    }
-
-    or
-
-    const user = await User.findById(id);
-    user.addresses.push({})...
-    ...
-*/
 
 module.exports = mongoose.model('Emergency', emergencySchema);
