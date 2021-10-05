bc5cff2be176a8ab1c2bdaf7b25e7aba44654459
sa <alvarengasergio@hotmail.co.uk>
2021-10-05 14:29:45 +0200

    added register functionality

--- Router/userRoutes.js
+++ Router/userRoutes.js
@@ -4,10 +4,37 @@ const ExpressError = require('../helpers/ExpressError');
 const User = require('../models/user');
 const methodOverride = require('method-override');
 const passport = require("passport");
+const {isLoggedIn} = require('../helpers/middleware');
 
 
 router.use(methodOverride('_method'));
 
+
+
+router.get('/register', isLoggedIn, (req,res)=>{
+    res.render('Users/register');
+});
+
+router.post('/register', isLoggedIn, catchAsync(async(req,res)=>{
+    try{
+
+        const {name, username, phone, address, city, password} = req.body;
+        let superAdmin;
+        if(req.body.superAdmin){
+            superAdmin = true;
+        }else{
+            superAdmin = false
+        }
+        const newAdmin = new User({name, username, phone, address, city, superAdmin});
+        const registeredUser = await User.register(newAdmin, password)
+        console.log(registeredUser);
+        req.flash('success', 'Successfully created a new user')
+        res.redirect('/volunteers');
+    }catch(err){
+        req.flash('error', err.message);
+        res.redirect('/register');
+    }
+}));
 
 
 router.get('/login', (req,res)=>{
--- models/user.js
+++ models/user.js
@@ -3,7 +3,25 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const {Schema} = mongoose;
 
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+    name: String,
+    phone: Number,
+    superAdmin:{
+        type: Boolean,
+        default: false
+    },
+    created:{
+        type: Date,
+        default: new Date()
+    },
+    address:{
+        type: String,
+    },
+    city: {
+        type: String
+    },
+
+});
 
 UserSchema.plugin(passportLocalMongoose);
 
