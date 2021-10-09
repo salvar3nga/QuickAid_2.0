ec1b92eadac648175cb35e8523f6e1d4ea399539
sa <alvarengasergio@hotmail.co.uk>
2021-10-09 15:51:05 +0200

    removed logs

--- Router/volunteerRoutes.js
+++ Router/volunteerRoutes.js
@@ -17,7 +17,6 @@ const validateVolunteer = (req, res, next)=>{
     const {error} = volunteerValidation.validate(req.body);
     
     if(error){
-        console.log(error);
         const msg = error.details.map(el=>el.message).join(',');
         throw new ExpressError(msg, 400);
     }else{
